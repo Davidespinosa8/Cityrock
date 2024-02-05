7f3194735e958aeae877c52685b1130346ebb8a8
davidespinosa8 <ddavidespinosa8@gmail.com>
2024-02-05 10:34:21 -0300

    correccion de precarga y color

--- assest/js/main.js
+++ assest/js/main.js
@@ -1,12 +1,9 @@
-$(window).on('load', function () {
-    // Ocultar la barra de navegación al inicio
+$(document).ready(function () {
     $('.navbar, #offcanvas').hide();
 
     setTimeout(function () {
         $('#onload').fadeOut();
         $('body').removeClass('hidden');
-
-        // Mostrar la barra de navegación después de ocultar el preloader
         $('.navbar, #offcanvas').show();
     }, 4000);
 });
